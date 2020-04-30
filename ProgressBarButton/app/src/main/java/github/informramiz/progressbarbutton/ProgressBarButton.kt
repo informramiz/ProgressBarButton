574e6183121a821600a2b8bd0eddd8c6d4796e2a
Ramiz Raja <informramiz@gmail.com>
2020-05-01 01:21:36 +0500

    feat: Create blank custom view button

---
+++ ProgressBarButton/app/src/main/java/github/informramiz/progressbarbutton/ProgressBarButton.kt
@@ -0,0 +1,16 @@
+package github.informramiz.progressbarbutton
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by Ramiz Raja on 01/05/2020
+ */
+class ProgressBarButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attrs, defStyle) {
+
+}
