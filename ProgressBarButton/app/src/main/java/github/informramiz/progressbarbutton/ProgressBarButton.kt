ac26fdee31200246b65e0bb7b51d1da6a78b7fe5
Ramiz Raja <informramiz@gmail.com>
2020-05-03 01:06:51 +0500

    feat: Write code to draw progress circle

--- ProgressBarButton/app/src/main/java/github/informramiz/progressbarbutton/ProgressBarButton.kt
+++ ProgressBarButton/app/src/main/java/github/informramiz/progressbarbutton/ProgressBarButton.kt
@@ -1,6 +1,9 @@
 package github.informramiz.progressbarbutton
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -13,4 +16,31 @@ class ProgressBarButton @JvmOverloads constructor(
     defStyle: Int = 0
 ) : View(context, attrs, defStyle) {
 
+    private var radius = 0f
+    private var circleInsetVertical = 0f
+    private var circleCenterY = 0f
+    private var circleCenterX = 0f
+
+    private val paint = Paint().apply {
+        isAntiAlias = true
+        color = Color.GRAY
+    }
+
+    init {
+        setBackgroundColor(Color.GREEN)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        radius = height / 3.5f
+        circleInsetVertical = (height - radius*2) / 2
+        circleCenterY = circleInsetVertical + radius
+        circleCenterX = width / 2f
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        canvas.drawCircle(circleCenterX, circleCenterY, radius, paint)
+    }
 }
